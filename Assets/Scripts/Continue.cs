97430110b00fd88897d6dc5f103d682be48df73f
Caleb Kauffman <Caleb Kauffman>
2016-04-14 10:50:05 -0700

    Splash screen commit

---
+++ Assets/Scripts/Continue.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Continue : MonoBehaviour {
+
+    public GameObject button, box, border, text;
+    public GameObject Freedom, Baby;
+    public Text freedom, baby;
+    public Image button1, button2;
+
+    void Start()
+    {
+        button = GameObject.Find("Intro button");
+        box = GameObject.Find("Intro square");
+        border = GameObject.Find("Intro Dialogue");
+        text = GameObject.Find("Intro message");
+    }
+
+    public void onClick()
+    {
+        button.SetActive(false);
+        box.SetActive(false);
+        border.SetActive(false);
+        text.SetActive(false);
+        freedom.enabled = true;
+        baby.enabled = true;
+        button1.enabled = true;
+        button2.enabled = true;
+    }
+}
---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class MenuScript : MonoBehaviour {
+
+    public GameObject dialogue, square, text;
+    public Renderer rend1, rend2;
+
+    void Start()
+    {
+        dialogue = GameObject.Find("Dialogue");
+        square = GameObject.Find("Square");
+    }
+
+    public void beABigBaby()
+    {
+        if (text.GetComponent<Text>().enabled)
+            text.GetComponent<Text>().enabled = false;
+
+        text = GameObject.Find("Message1");
+        rend1 = dialogue.GetComponent<SpriteRenderer>();
+        rend1.enabled = true;
+        rend2 = square.GetComponentInChildren<SpriteRenderer>();
+        rend2.enabled = true;
+
+        text.GetComponent<Text>().enabled = true;
+
+        //System.Threading.Thread.Sleep(5000);
+        //Application.Quit();
+    }
+
+    public void Freedom()
+    {
+        if (text.GetComponent<Text>().enabled)
+            text.GetComponent<Text>().enabled = false;
+
+        text = GameObject.Find("Message2");
+        rend1 = dialogue.GetComponent<SpriteRenderer>();
+        rend1.enabled = true;
+        rend2 = square.GetComponentInChildren<SpriteRenderer>();
+        rend2.enabled = true;
+
+        text.GetComponent<Text>().enabled = true;
+    }
+
+    private Vector3 screenPoint;
+    private Vector3 offset;
+
+    void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 cursorPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(cursorPoint) + offset;
+        transform.position = cursorPosition;
+
+     }
+    void Update()
+    {
+
+    }
+}
