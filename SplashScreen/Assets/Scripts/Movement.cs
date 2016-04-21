d3e8a87e74047f182dcf71c6544c9b6136d345ea
Caleb Kauffman <Caleb Kauffman>
2016-04-21 06:59:11 -0700

    Added basic movement, main map

---
+++ SplashScreen/Assets/Scripts/Movement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    public float speed = 1.0f;
+    //public Animator WalkRight;
+    //public Animation WalkUp;
+
+    void Update()
+    {
+        var initial = transform.position;
+        var end = transform.position;
+        if (Input.GetKey(KeyCode.A))
+        {
+            end = transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            end = transform.position += Vector3.right * speed * Time.deltaTime;
+            //WalkRight.enabled = true;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            end = transform.position += Vector3.up * speed * Time.deltaTime;
+           // WalkUp.enabled = true;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            end = transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+
+        if(initial == end)
+        {
+            //WalkRight.enabled = false;
+        }
+    }
+}
